e8cfd4168945c908993158a604ae07877bc370f0
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-07 02:14:19 +0600

    get input value and calculate total function added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 function setPrice(product, isIncreasing, number) {
     const productCost = document.getElementById(product + '-cost')
-    // productCost.innerText = 0;
-    // const productCostNumber = parseInt(productCost.innerText);
-    // console.log(productCostNumber)
 
     if (isIncreasing == false) {
         productCost.innerText = number;
@@ -10,43 +7,54 @@ function setPrice(product, isIncreasing, number) {
     else if (isIncreasing == true) {
         productCost.innerText = number;
     }
+    calculateTotal();
+
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-cost');
+    const productInputNumber = parseFloat(productInput.innerText);
+    return productInputNumber;
+}
+
+function calculateTotal() {
+    const bestPrice = getInputValue('minimum');
+    const extraMemoryCost = getInputValue('memory')
+    const extraStorageCost = getInputValue('storage');
+    const deliveryCharge = getInputValue('delivery');
+
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = bestPrice + extraMemoryCost + extraStorageCost + deliveryCharge;
+
+    const finalTotalPrice = document.getElementById('final-total');
+    finalTotalPrice.innerText = bestPrice + extraMemoryCost + extraStorageCost + deliveryCharge;
+
 }
 
 
 document.getElementById('8GB-memory').addEventListener('click', function () {
-    // const productCost = document.getElementById('memory-cost')
-    // productCost.innerText = 0;
     setPrice('memory', false, 0)
+
 })
 
 
 document.getElementById('16GB-memory').addEventListener('click', function () {
-    // const productCost = document.getElementById('memory-cost')
-    // productCost.innerText = 180;
-
     setPrice('memory', true, 180)
+
 })
 
 document.getElementById('256GB-ssd').addEventListener('click', function () {
-    // const storageCost = document.getElementById('storage-cost');
-    // storageCost.innerText = 0;
     setPrice('storage', false, 0)
 })
 document.getElementById('512GB-ssd').addEventListener('click', function () {
-    // const storageCost = document.getElementById('storage-cost');
-    // storageCost.innerText = 100;
 
     setPrice('storage', true, 100)
 })
 
 document.getElementById('free-delivery').addEventListener('click', function () {
-    // const deliveryCost = document.getElementById('delivery-cost');
-    // deliveryCost.innerText = 0;
     setPrice('delivery', false, 0)
 })
 
 document.getElementById('paid-delivery').addEventListener('click', function () {
-    // const deliveryCost = document.getElementById('delivery-cost');
-    // deliveryCost.innerText = 20;
     setPrice('delivery', true, 20)
 })
